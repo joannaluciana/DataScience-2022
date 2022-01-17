811e435d3bbd2ec79bf82c33cc379060bd5b1ead
Joanna <joannaluciana>
2022-01-17 13:58:49 +0100

    Project Data Science

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
 
+
+app.layout
+
 app = dash.Dash(__name__)
 if __name__ == '__main__':
     app.run_server(debug=True)
